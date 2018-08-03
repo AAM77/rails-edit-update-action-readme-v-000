d48786db368b43487db01b540f9e16bf547a080c
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-03 12:57:41 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 	end
 	
 	def update
-	  
+	  raise params.inspect
 	  #@post = Post.find_by_id(params[:id])
 	  #@post.title = params[:post][:title]
 	  #@post.description = params[:post][:description]
